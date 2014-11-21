92c93f62e2ba326df9ab4ff4a106c2f88c2c6850
JHonaker <honaker215@gmail.com>
2014-11-21 18:21:53 -0500

    Added test for proper castling

--- tests/testthat/test_moveparser.R
+++ tests/testthat/test_moveparser.R
@@ -36,5 +36,21 @@ test_that("parse.move properly captures pieces", {
 })
 
 test_that("parse.move deals with castling", {
+    startpos <- list(Wking = 'e1', WLrook = 'a1', WRrook = 'h1',
+                     Bking = 'e8', BLrook = 'a8', BRrook = 'h8')
+    WKcastle <- list(Wking = 'g1', WLrook = 'a1', WRrook = 'f1',
+                     Bking = 'e8', BLrook = 'a8', BRrook = 'h8')
+    WQcastle <- list(Wking = 'c1', WLrook = 'd1', WRrook = 'h1',
+                     Bking = 'e8', BLrook = 'a8', BRrook = 'h8')
+    BKcastle <- list(Wking = 'e1', WLrook = 'a1', WRrook = 'h1',
+                     Bking = 'g8', BLrook = 'a8', BRrook = 'f8')
+    BQcastle <- list(Wking = 'e1', WLrook = 'a1', WRrook = 'h1',
+                     Bking = 'c8', BLrook = 'd8', BRrook = 'h8')
 
+    move_to <- function(move) parse.move(startpos, move)
+
+    expect_equal(move_to('e1g1'), WKcastle) # And e1 is white king
+    expect_equal(move_to('e1c1'), WQcastle) # And e1 is white king
+    expect_equal(move_to('e8g8'), BKcastle) # And e8 is black king
+    expect_equal(move_to('e8c8'), BQcastle) # And e8 is black king
 })
